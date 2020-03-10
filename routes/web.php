f78dd2e7f0bb03c7e8f7e4d03dbe3d9fb42ec863
Joan Henry <jhenry081@gmail.com>
2020-03-10 11:49:40 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,17 @@
 |
 */
 
-Route::get('/','OtherPagesController@index');
+Route::get('/', 'OtherPagesController@index');
 //____________________________AUTH USER_______________________________________________
-Route::get('/auth/register','AuthUserController@register');
-Route::get('/auth/login','AuthUserController@login');
-Route::post('/auth/register','AuthUserController@store')->name('auth.register');
-Route::post('/auth/login','AuthUserController@loginChecking')->name('auth.login');
+Route::get('/auth/register', 'AuthUserController@register');
+Route::get('/auth/login', 'AuthUserController@login');
+Route::post('/auth/register', 'AuthUserController@store')->name('auth.register');
+Route::post('/auth/login', 'AuthUserController@loginChecking')->name('auth.login');
 
 
 //APPLICATION ROUTES
-Route::get('/apply/home/user','ApplicationController@index')->name('apply.home');
-Route::get('/applicant/user/basic-info','ApplicationController@basicInfo')->name('applicant.basic-info');
+Route::get('/apply/home/user', 'ApplicationController@index')->name('apply.home');
+Route::get('/applicant/user/basic-info', 'ApplicationController@basicInfo')->name('applicant.basic-info');
 //Route::get('/applicant/user/education-level','ApplicationController@index')->name('applicant.basic-info');
 //Route::get('/applicant/user/closest-relative','ApplicationController@index')->name('applicant.basic-info');
 //Route::get('/applicant/user/attachments','ApplicationController@index')->name('applicant.basic-info');
